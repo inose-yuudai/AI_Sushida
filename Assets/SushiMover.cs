c02790c79af8bcaea1f34dd6e8564f1a6f66cfee
inose-yuudai <inobee1412@yahoo.co.jp>
2024-02-23 00:39:55 +0900

    寿司が消える条件を修正

--- Assets/SushiMover.cs
+++ Assets/SushiMover.cs
@@ -5,31 +5,34 @@ public class SushiMover : MonoBehaviour
     public float speed = 5.0f; // 寿司の移動速度
     private RectTransform rectTransform; // ImageのRectTransform
     private Canvas canvas; // 親のCanvas
-    private float timer = 0f; // タイマー
 
-    void Start()
+ void Start()
+{
+    // 2階層上のCanvasを取得
+    canvas = GetComponentInParent<Canvas>(); // まず直近のCanvasを取得
+    if (canvas == null || !canvas.gameObject.name.Equals("Canvas")) // 目的のCanvasでなければさらに上を探す
     {
-        rectTransform = GetComponent<RectTransform>(); // ImageのRectTransformを取得
-        canvas = GetComponentInParent<Canvas>(); // 親のCanvasを取得
+        Transform parentCanvasTransform = transform.parent.parent; // 2階層上を参照
+        canvas = parentCanvasTransform.GetComponent<Canvas>(); // 2階層上のCanvasを取得
     }
+
+    rectTransform = GetComponent<RectTransform>(); // ImageのRectTransformを取得
+}
 
     void Update()
     {
         // 寿司を右に移動させる
         transform.Translate(speed * Time.deltaTime * 20, 0, 0);
 
-        // タイマーを更新
-        timer += Time.deltaTime;
+        // スクリーンポイントを取得
+        Vector3 screenPoint = RectTransformUtility.WorldToScreenPoint(canvas.worldCamera, rectTransform.position);
 
-        // 8秒経過したかをチェック
-        if (timer >= 7f)
+        // 画面外に出たかどうかをチェック
+        if (screenPoint.x < 0 || screenPoint.x > Screen.width)
         {
-            // 8秒経過したら、GameManagerのDestroySushiとSpawnSushiメソッドを呼び出す
+            // 画面外に出たら、GameManagerのDestroySushiとSpawnSushiメソッドを呼び出す
             GameObject.Find("GameManager").SendMessage("DestroySushi");
             GameObject.Find("GameManager").SendMessage("SpawnSushi");
-
-            // タイマーをリセット
-            timer = 0f;
         }
     }
 }
